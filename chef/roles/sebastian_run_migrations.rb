0ecb664f6db5b05eb4d2cf0520b6451ab4b709b2
pitr <pitr.vern@gmail.com>
2012-11-05 22:44:32 -0500

    remove old chef roles and databags

--- chef/roles/sebastian_run_migrations.rb
+++
@@ -1,3 +0,0 @@
-name "sebastian_run_migrations"
-description "Run db:migrate on demand for sebastian"
-override_attributes( "apps" => { "sebastian" => { "production" => { "run_migrations" => true } } })
